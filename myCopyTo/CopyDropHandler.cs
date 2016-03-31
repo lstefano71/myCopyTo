69d1a3bfc88e766835ef77e60c290e6770f7b1ac
stf@apl.it <stf@apl.it>
2016-03-31 19:33:02 +0200

    Add project files.

---
+++ myCopyTo/CopyDropHandler.cs
@@ -0,0 +1,91 @@
+﻿using SharpShell.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myCopyTo
+{
+	[ComVisible(true)]
+	[COMServerAssociation(AssociationType.FileExtension,".mycopy")]
+	class CopyDropHandler : SharpShell.SharpDropHandler.SharpDropHandler
+	{
+		protected override void DragEnter(DragEventArgs dragEventArgs)
+		{
+			dragEventArgs.Effect = DragDropEffects.Copy;
+		}
+
+		protected override void Drop(DragEventArgs dragEventArgs)
+		{
+			var target = Path.GetDirectoryName(SelectedItemPath);
+			target = Path.Combine(target, Path.GetFileNameWithoutExtension(SelectedItemPath));
+			var frm = new frmProgress();
+			frm.Show();
+			frm.prgBar.Minimum = 1;
+			frm.prgBar.Maximum = DragItems.Count();
+			var c = 1;
+			// TODO: spawn a separate thread for GUI 
+			// TOOD: spawn a separate thread for actual copy
+			foreach(var source in DragItems) {
+				frm.prgBar.Value = c++;
+				frm.Text = source;
+				var a = File.GetAttributes(source);
+				if (a.HasFlag(FileAttributes.Directory)) {
+					var t = new DirectoryInfo(Path.Combine(target, Path.GetFileName(source)));					
+					CopyFilesRecursively(new DirectoryInfo(source), t);
+				} else
+					File.Copy(source, Path.Combine(target, Path.GetFileName(source)));
+			}
+			frm.Close();
+		}
+		static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+		{
+			var od = source.LastWriteTimeUtc;
+			target.Create();
+			foreach (DirectoryInfo dir in source.GetDirectories()) {
+				try {
+					CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
+				} catch (Exception ex) {
+					MessageBox.Show(ex.ToString());
+				}				
+			}
+			foreach (FileInfo file in source.GetFiles()) {				
+				try {
+					CopyTo(file, Path.Combine(target.FullName, file.Name));
+				} catch(Exception ex) {
+					MessageBox.Show(ex.ToString());
+				}
+			}
+			target.LastWriteTimeUtc = od;
+			source.LastAccessTimeUtc = od;
+		}
+
+		private static void CopyTo(FileInfo source, string target)
+		{
+			var od = source.LastWriteTimeUtc;
+			var on = source.FullName;
+			source.MoveTo(on + ".xxx");
+			source.Refresh();
+			var tot = source.Length;
+			var buflen = 8192;
+			var buf = new byte[buflen];			
+			using (var s = source.OpenRead())
+			using (var t = File.OpenWrite(target)) {
+				t.SetLength(tot);
+				t.Seek(0, SeekOrigin.Begin);
+				while(tot>0) {
+					var toread = (int) Math.Min(buflen, tot);
+					var r = s.Read(buf, 0, toread);
+					t.Write(buf, 0, r);
+					tot -= r;
+				}
+			}
+			File.SetLastWriteTimeUtc(target, od);
+			source.MoveTo(on);
+		}
+	}
+}
---
+++ myCopyTo/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace myCopyTo
+{
+	partial class frmProgress
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.prgBar = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// prgBar
+			// 
+			this.prgBar.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.prgBar.Location = new System.Drawing.Point(0, 0);
+			this.prgBar.Name = "prgBar";
+			this.prgBar.Size = new System.Drawing.Size(593, 35);
+			this.prgBar.TabIndex = 0;
+			this.prgBar.UseWaitCursor = true;
+			// 
+			// frmProgress
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(593, 35);
+			this.Controls.Add(this.prgBar);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "frmProgress";
+			this.UseWaitCursor = true;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		public System.Windows.Forms.ProgressBar prgBar;
+	}
+}
+
